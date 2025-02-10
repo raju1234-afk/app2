38e09f37196312f2dd78c50ea7a7a6aa7df9d335
kanna <bathula.kanna@gmail.com>
2025-02-10 14:50:17 +0530

    p4

---
+++ Experiment - 2/app.py
@@ -0,0 +1,25 @@
+  # Task: User Registration flask application.
+     # Date:
+     # File Name: app.py
+
+     # Author_Name: Write the developer’s name(M. Venkateswarl)
+
+       # Import the Flask and OS python modules 
+from flask import Flask,render_template
+from flask import request,redirect,url_for
+import os
+app=Flask(__name__)
+@app.route("/")
+def home():
+    return render_template('Registration.html')
+@app.route('/register', methods=['GET', 'POST']) 
+def register():
+     if request.method == 'POST': 
+            name = request.form['name'] 
+            email = request.form['email']
+            password = request.form['password']
+            # Store the user data in a database or file
+            return render_template('Success.html')
+if __name__ == "__main__":
+ port = int(os.environ.get('PORT', 5000))
+ app.run(debug=True, host='0.0.0.0', port=port)
